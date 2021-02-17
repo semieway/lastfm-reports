5e764159c24573289a56cbe9310e0774ca03c79c
Alex Sirotin <semieway@gmail.com>
2021-02-17 18:59:11 +0500

    Create user class

---
+++ src/User.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Semieway\Lastfmreports;
+
+
+class User
+{
+    /** @var string */
+    protected $userName;
+
+    /** @var string */
+    protected $userPic;
+
+    /** @var string */
+    protected $artists;
+
+    /** @var string */
+    protected $albums;
+
+    /** @var string */
+    protected $tracks;
+
+
+}
